0cf8d05776e1c65357cf82376f90cd850bd073c4
Chris Underwood <cunderwoodmn@gmail.com>
2016-10-15 19:48:37 -0700

    add button to cover spoilers

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 //Prevent spoilerphobes from seeing spoilers
 //Solution: Hide spoilers and reveal them through user interaction
 
+// 1. Add spoilers
+$(".spoiler span").hide();
+// 2. Add button
+$(".spoiler").append('<button>Reveal Spoiler!</button>');
